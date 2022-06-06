6211b38c7841a4a0126727eb43cab3c98f127d73
Jhong <jhong2792@gmail.com>
2022-06-06 15:06:44 -0500

    added basic nav bar

---
+++ data/work.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+      title: 'Tic-Tac-Toe',
+      image: '',
+      description: '2 users placy a game of tic-tac-toe.',
+      githubLink: 'https://github.com/thejhong/ttt-weekend.git',
+      deploymentLink: 'https://aquamarine-chimera-21e54c.netlify.app'
+  }
+
+]
